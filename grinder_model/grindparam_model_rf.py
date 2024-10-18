822058ed105f15b6d1e5e79190570a1a47d2664b
panin-ananwa <pananwatanyoo@tudelft.nl>
2024-10-18 10:42:03 +0000

    change save/load path

--- grinder_model/grindparam_model_rf.py
+++ grinder_model/grindparam_model_rf.py
@@ -147,7 +147,7 @@ def open_file_dialog():
 
 def save_model(model, scaler, folder_name='saved_models', modelname='svr_model.pkl', scalername='scaler.pkl'):
     # Get the current working directory
-    current_dir = os.getcwd()
+    current_dir = pathlib.Path.cwd() / 'src' / 'grinder_model'
 
     # Create the full path by joining the current directory with the folder name
     folder_path = os.path.join(current_dir, folder_name)
@@ -167,7 +167,7 @@ def save_model(model, scaler, folder_name='saved_models', modelname='svr_model.p
 
 def load_model(folder_name='saved_models', filename='svr_model.pkl'):
     # Get the current working directory
-    current_dir = os.getcwd()
+    current_dir = pathlib.Path.cwd() / 'src' / 'grinder_model'
 
     # Create the full path by joining the current directory with the folder name
     folder_path = os.path.join(current_dir, folder_name)
